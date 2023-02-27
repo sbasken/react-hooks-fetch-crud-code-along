85c1323a9661275a52b367a21f3cc3444533304f
Saki <sbasken@gmail.com>
2023-02-26 17:38:14 -0800

    fetch items using useEffect

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,13 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then(res => res.json())
+      .then(items => setItems(items))
+      
+  }, [])
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
